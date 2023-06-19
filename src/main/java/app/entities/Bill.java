c67f95a869e03c8db9d88d8f403c8bcb7bb8af43
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-19 14:16:02 +0100

    chenged long to uuid in class bill

--- src/main/java/app/entities/Bill.java
+++ src/main/java/app/entities/Bill.java
@@ -2,8 +2,11 @@ package app.entities;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,18 +19,31 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "bills")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Bill {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	@GeneratedValue
+	private UUID id;
 	private Integer year;
 	private Date date;
 	private BigDecimal amount;
 	private Integer number;
+	@Enumerated(EnumType.STRING)
 	private StatusBill statusBill;
 	
-	//@ManyToOne
-	//private User user;
+	
+	@ManyToOne
+	private User user;
+
+	public Bill(Integer year, Date date, BigDecimal amount, Integer number, StatusBill statusBill, User user) {
+		super();
+		this.year = year;
+		this.date = date;
+		this.amount = amount;
+		this.number = number;
+		this.statusBill = statusBill;
+		this.user = user;
+	}
+	
+	
 }
