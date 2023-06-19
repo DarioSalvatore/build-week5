5b385d850aac207bfb0a3ae3b7a82c1ca5c37457
DarioSalvatore <dariopay69@gmail.com>
2023-06-19 12:22:34 +0200

    inizializzazione

---
+++ src/main/java/app/BuildWeek5Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeek5Application.class, args);
+	}
+
+}
