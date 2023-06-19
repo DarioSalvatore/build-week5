0cf6813a14d41b57f4e0065f4bd06403fdf927d5
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-19 13:43:46 +0100

    adds bills class

---
+++ src/main/java/app/entities/Bill.java
@@ -0,0 +1,27 @@
+package app.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bills")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bill {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private Integer year;
+	private Date date;
+	private BigDecimal amount;
+	private Integer number;
+	private StatusBill statusBill;
+}
