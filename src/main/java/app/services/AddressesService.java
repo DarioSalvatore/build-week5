d1de285d3670b4c46805fe0f4a5798a5b29b59bb
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-06-20 09:21:43 +0200

    Addresses feature completed

--- src/main/java/app/services/AddressesService.java
+++ src/main/java/app/services/AddressesService.java
@@ -7,6 +7,7 @@ import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Component;
 
 import app.entities.Address;
+import app.payloads.AddressPayload;
 import app.repositories.AddressesRepository;
 
 @Component
@@ -17,5 +18,27 @@ public class AddressesService {
 	public Address findById(UUID id) throws NotFoundException {
 		return addressesRepo.findById(id)
 				.orElseThrow(() -> new app.exceptions.NotFoundException("Indirizzo non trovato!"));
+	}
+
+	public Address create(AddressPayload p) {
+		Address address = new Address(p.getVia(), p.getCivico(), p.getLocalità(), p.getCap(), p.getTipo(), p.getUser(),
+				p.getComune());
+		return address;
+	}
+
+	public Address findByIdAndUpdate(UUID id, AddressPayload p) throws NotFoundException {
+		Address found = this.findById(id);
+		found.setId(found.getId());
+		found.setVia(p.getVia());
+		found.setCivico(p.getCivico());
+		found.setLocalità(p.getLocalità());
+		found.setCap(p.getCap());
+		found.setTipo(p.getTipo());
+		found.setComune(p.getComune());
+		return addressesRepo.save(found);
+	}
+
+	public void deleteAddress(UUID id) {
+		addressesRepo.deleteById(id);
 	}
 }
