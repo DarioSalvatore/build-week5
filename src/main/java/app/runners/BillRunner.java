b0fd3dc76d2e27ecac67fd999a75f554b51ae801
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-06-23 14:09:04 +0200

    Fixato problema anno fatture

--- src/main/java/app/runners/BillRunner.java
+++ src/main/java/app/runners/BillRunner.java
@@ -28,31 +28,6 @@ public class BillRunner implements CommandLineRunner {
 	@Autowired
 	UserRepository userRepo;
 
-//	@Override
-//	public void run(String... args) throws Exception {
-//		List<User> users = userRepo.findAll();
-//		Faker faker = new Faker(new Locale("it"));
-//		Random random = new Random();
-//		for (int i = 0; i < 50; i++) {
-//			try {
-//				Integer year = faker.number().numberBetween(1900, 2023);
-//				LocalDate date = faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-//				BigDecimal amount = new BigDecimal(faker.number().randomDouble(2, 0, 1000)); // da 0 a 1000 con 2 cifre
-//																								// decimali
-//				Integer number = random.nextInt();
-//				StatusBill statusBill = faker.options().nextElement(StatusBill.values());
-//
-//				User user = new User();
-//
-//				Bill bill = new Bill(year, date, amount, number, statusBill, user);
-//				billService.create(bill);
-//
-//			} catch (Exception e) {
-//				System.out.println(e);
-//			}
-//		}
-//
-//	}
 	@Override
 	public void run(String... args) throws Exception {
 		List<User> users = userRepo.findAll();
@@ -62,14 +37,13 @@ public class BillRunner implements CommandLineRunner {
 		if (billService.count() == 0) {
 			for (User user : users) {
 				try {
-					int numberOfBills = random.nextInt(10) + 1; // Genera un numero casuale compreso tra 1 e 10
+					int numberOfBills = random.nextInt(10) + 1;
 
 					for (int i = 0; i < numberOfBills; i++) {
-						Integer year = faker.number().numberBetween(1900, 2023);
 						LocalDate date = faker.date().birthday().toInstant().atZone(ZoneId.systemDefault())
 								.toLocalDate();
+						Integer year = date.getYear();
 						BigDecimal amount = new BigDecimal(faker.number().randomDouble(2, 0, 1000));
-//						Integer number = random.nextInt();
 						StatusBill statusBill = faker.options().nextElement(StatusBill.values());
 
 						Bill bill = new Bill(year, date, amount, statusBill, user);
